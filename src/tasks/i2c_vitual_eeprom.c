66572639d4ff8df55ddc03d09bf54f9a1581e3aa
Felix Huebner <huebner@unipoin.com>
2019-08-29 12:13:03 +0200

    cleanup and removal of debug outputs. add method descriptions.

--- src/tasks/i2c_vitual_eeprom.c
+++ src/tasks/i2c_vitual_eeprom.c
@@ -68,104 +68,97 @@ void configure_i2c_slave(i2c_module_t *i2c_module, uint8_t slave_address) {
 }
 
 
+/**
+ * start a i2c read job
+ * @param i2c_module i2c_module instance object
+ * @param length length of data to be read
+ * @param ptr pointer to a data region to write data to
+ * @return return STATUS_OK if successful or STATUS_BUSY if the read job could not be set up
+ */
 enum status_code _i2c_receive_data(i2c_module_t *i2c_module, uint8_t length, uint8_t *ptr) {
 	data_packet_ptr->data_length = length;
 	data_packet_ptr->data = ptr;
 	return i2c_slave_read_packet_job(i2c_module, data_packet_ptr);
 }
 
+
+/**
+ * start a i2c write job
+ * @param i2c_module i2c_module instance object
+ * @param length length of data to be written
+ * @param ptr pointer to a data region to read data from
+ * @return return STATUS_OK if successful or STATUS_BUSY if the write job could not be set up
+ */
 enum status_code _i2c_send_data(i2c_module_t *i2c_module, uint8_t length, uint8_t *ptr) {
 	data_packet_ptr->data_length = length;
 	data_packet_ptr->data = ptr;
 	return i2c_slave_write_packet_job(i2c_module, data_packet_ptr);
 }
 
+
 /**
- * request to read from master
+ * request to read from master, we allow only 1 byte (data address)
  * @param module
  */
 void i2c_callback_master_read_request(i2c_module_t *module) {
 	port_pin_set_output_level(PIN_PA20, true);
-	c_log_s(" r-");
-	port_pin_set_output_level(PIN_PA20, false);
-
-	uint32_t status = i2c_slave_get_status(module);
-	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status)) + 0x30));
-//TODO this make no sense, write will not use a repeated start to write to device!! Fix later
-	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("-S1");
-		//calculate allowed bytes to write
-		uint8_t sum = 0;
-		if (sum > 0) {
-			enum status_code stat = _i2c_receive_data(module, sum, &i2c_data_address); // teporarilly
-			if (stat == STATUS_BUSY) {
-				c_log_s("-E_B");
-			}
-		} else {
-			//i2c_slave_enable_nack_on_address(module);
-		}
-	} else {
-		//start read 1 byte (command/address byte)
-		enum status_code stat = _i2c_receive_data(module, 1, &i2c_data_address);
-		if (stat == STATUS_BUSY) {
-			c_log_s("-E_B");
-		}
-//		i2c_slave_disable_nack_on_address(module);
+//	c_log_s(" r-");
+//	uint32_t status = i2c_slave_get_status(module);
+//	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status)) + 0x30));
+	//start read 1 byte (command/address byte)
+	enum status_code stat = _i2c_receive_data(module, 1, &i2c_data_address);
+	if (stat == STATUS_BUSY) {
+//		c_log_s("-E_B");
 	}
-}
-
-
-void i2c_callback_slave_read_complete(i2c_module_t *module) {
-	port_pin_set_output_level(PIN_PA20, true);
-	c_log_s(" R-");
 	port_pin_set_output_level(PIN_PA20, false);
-	uint32_t status = i2c_slave_get_status(module);
-	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status)) + 0x30));
-	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("-S4");
-	}
 }
 
 
 /**
- * master requests at least 1 byte
- * @param module
+ * will be called if read from master complete
+ * @param module i2c_module instance object
+ */
+void i2c_callback_slave_read_complete(i2c_module_t *module) {
+	port_pin_set_output_level(PIN_PA20, true);
+//	c_log_s(" R-");
+	uint32_t status = i2c_slave_get_status(module);
+//	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status)) + 0x30));
+	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
+//		c_log_s("-S4");
+	}
+	port_pin_set_output_level(PIN_PA20, false);
+}
+
+
+/**
+ * request to write to master, we don't know the length, so we start a job with the maximal possible length (up to the valid end of the eeprom)
+ * @param module i2c_module instance object
  */
 void i2c_callback_master_write_request(i2c_module_t *module) {
 	port_pin_set_output_level(PIN_PA20, true);
-	c_log_s(" w-");
-	port_pin_set_output_level(PIN_PA20, false);
+//	c_log_s(" w-");
+
 
 	uint32_t status = i2c_slave_get_status(module);
-	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status)) + 0x30));
 	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("-S3");
-
-		//calculate allowed bytes to write
-		uint8_t sum = 0;
-		if (sum > 0) {
-			enum status_code stat = _i2c_receive_data(module, sum, &i2c_data_address); // teporarilly
-			if (stat == STATUS_BUSY) {
-				c_log_s("-E_B");
-			}
-		} else {
-//TODO why change nack/ack on address reception? this is a disable of the i2c interface on the specified address, is it?
-
-//			i2c_slave_enable_nack_on_address(module);
-		}
+//		c_log_s("-S3");
 	}
+
 	uint8_t length = 0;
+	//check if the data address is inside the valid eeprom space
 	if (i2c_data_address < EEPROM_USED_SIZE) {
+		//calculate allowed bytes to send
 		length = EEPROM_USED_SIZE - i2c_data_address;
 		enum status_code stat = _i2c_send_data(module, length, &(eeprom_data_ptr->array[i2c_data_address]));
 		if (stat == STATUS_BUSY) {
-			c_log_s("-E_B");
+//			c_log_s("-E_B");
 		}
 	} else {
 		//start a single write to act like an eeprom. the single byte will be send and then the interrupts are disabled
@@ -173,68 +166,89 @@ void i2c_callback_master_write_request(i2c_module_t *module) {
 		uint8_t tmp = 0xff;
 		enum status_code stat = _i2c_send_data(module, 1, &(tmp));
 		if (stat == STATUS_BUSY) {
-			c_log_s("-E_B");
+//			c_log_s("-E_B");
 		}
 	}
+	port_pin_set_output_level(PIN_PA20, false);
 }
 
+
 /**
- * receive complete
- * @param module
+ * will be called if write to master is complete
+ * @param module i2c_module instance object
  */
 void i2c_callback_slave_write_complete(i2c_module_t *module) {
 	port_pin_set_output_level(PIN_PA20, true);
-	c_log_s(" W-");
-
+//	c_log_s(" W-");
 	uint32_t status = i2c_slave_get_status(module);
-	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status)) + 0x30));
-	port_pin_set_output_level(PIN_PA20, false);
+//	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status)) + 0x30));
 	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("-S2");
+//		c_log_s("-S2");
 	}
-//	i2c_slave_disable_nack_on_address(i2c_slave_instance);
+	port_pin_set_output_level(PIN_PA20, false);
 }
 
+
+/**
+ * will be called if a transfer error occured
+ * @param module i2c_module instance object
+ */
 void i2c_callback_transfer_error(i2c_module_t *module) {
 	port_pin_set_output_level(PIN_PA20, true);
-	c_log_s(" T-");
-	port_pin_set_output_level(PIN_PA20, false);
-
+//	c_log_s(" T-");
 	uint32_t status = i2c_slave_get_status(module);
-	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
-	c_log((uint8_t) ((0x0F & (status)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>8)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status>>4)) + 0x30));
+//	c_log((uint8_t) ((0x0F & (status)) + 0x30));
 	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("-S5");
+//		c_log_s("-S5");
 	}
-//	i2c_slave_disable_nack_on_address(i2c_slave_instance);
+	port_pin_set_output_level(PIN_PA20, false);
 }
 
+
+/**
+ * will be called if a error with a callback occured
+ * @param module i2c_module instance object
+ */
 void i2c_callback_error(i2c_module_t *module) {
 	port_pin_set_output_level(PIN_PA20, true);
-	c_log('E');
-	port_pin_set_output_level(PIN_PA20, false);
-
+//	c_log('E');
 	uint32_t status = i2c_slave_get_status(module);
 	if (status & I2C_SLAVE_STATUS_REPEATED_START) {
-		c_log_s("S6");
+//		c_log_s("S6");
 	}
-//	i2c_slave_disable_nack_on_address(i2c_slave_instance);
+	port_pin_set_output_level(PIN_PA20, false);
 }
 
+
 /******** Getter/Setter ********/
+
+/**
+ * increment the data counter in the eeprom
+ */
 void inc_adc_data_counter(void) {
 	if (eeprom_data_ptr) {
 		eeprom_data_ptr->s.adc_counter++;
 	}
 }
 
+
+/**
+ * pass a pointer of the eeprom data object to this object
+ * @param ptr pointer of the eeprom object
+ */
 void set_eeprom_data_pointer(adc_i2c_eeprom_u *ptr){
 	eeprom_data_ptr = ptr;
 }
 
+
+/**
+ * pass a pointer of the i2c_data packet to this object
+ * @param ptr pointer of the eeprom object
+ */
 void set_i2c_slave_data_packet(i2c_slave_packet_t *ptr) {
 	data_packet_ptr = ptr;
 }
