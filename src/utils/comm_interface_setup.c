9247d9bfcd009f7032cd5527ba1c605e1d53dd67
Felix Huebner <huebner@unipoin.com>
2019-11-01 12:50:47 +0100

    rename debug uart interface

--- src/utils/comm_interface_setup.c
+++ src/utils/comm_interface_setup.c
@@ -11,12 +11,12 @@ void configure_log_uart(usart_module_t *usart_module) {
     usart_get_config_defaults(&config_usart);
     config_usart.baudrate = 115200;
     config_usart.generator_source = GCLK_GENERATOR_1;
-	config_usart.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
-	config_usart.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
-	config_usart.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
-	config_usart.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
-	config_usart.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
-	while (usart_init(usart_module, EDBG_CDC_MODULE, &config_usart) != STATUS_OK) {}
+	config_usart.mux_setting = DEBUG_UART_MUX_SETTING;
+	config_usart.pinmux_pad0 = DEBUG_UART_PINMUX_PAD0;
+	config_usart.pinmux_pad1 = DEBUG_UART_PINMUX_PAD1;
+	config_usart.pinmux_pad2 = DEBUG_UART_PINMUX_PAD2;
+	config_usart.pinmux_pad3 = DEBUG_UART_PINMUX_PAD3;
+	while (usart_init(usart_module, DEBUG_UART_MODULE, &config_usart) != STATUS_OK) {}
     usart_enable(usart_module);
 }
 
