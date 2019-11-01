784825ff0cac3a231a25b11606098d2c56c5c2e9
Felix Huebner <huebner@unipoin.com>
2019-11-01 19:32:40 +0100

    add method descriptions for can_methods.c

--- src/tasks/can_methods.h
+++ src/tasks/can_methods.h
@@ -12,7 +12,6 @@ typedef struct {
 } can_flags_t;
 
 bool check_and_transfer_can_message_to_uart(struct can_module *const can_module, uint8_t cantask_id, uint8_t *sequence_counter);
-
 void reset_can_errorflags(can_flags_t *CAN_flags);
 void setup_can_instance(struct can_module *can_module, Can *can_hw, uint32_t bitrate);
 uint8_t transmit_CAN(struct can_module *const can_module, struct can_tx_element *tx_element);
